161fe8f15f0302b841d470e794a71a43312903f4
krisss2121 <krzysiekplachno@gmail.com>
2014-04-28 20:42:17 +0200

    MyGraph2 & Vertex2 - all works fine... At least I suppose...

--- LPP/src/myPkg/MyGraph2.java
+++ LPP/src/myPkg/MyGraph2.java
@@ -10,6 +10,9 @@ public class MyGraph2 {
 	
 	int tmpDistance = 0, tmpMinSumDistance = Integer.MAX_VALUE;
 	
+	/**
+	 * @param size
+	 */
 	MyGraph2(int size){
 		tableOfVertexes = new Vertex2[size];
 		/*
@@ -23,20 +26,22 @@ public class MyGraph2 {
 					// these distances has been already generated
 					tmpDistance = tableOfVertexes[currNeigh].getDistanceToNeigbour(currVer);
 					tableOfVertexes[currVer].setDistanceToNeighbour(currNeigh, tmpDistance);
-				}
-				if(currNeigh==currVer){
+				}else if(currNeigh==currVer){
 					// distance to itself is 0
 					tableOfVertexes[currVer].setDistanceToNeighbour(currNeigh, 0);
 				}else{
 					//we need to retain 'triangle inequality'
-					tmpMinSumDistance = Integer.MAX_VALUE;
-					for(int x=0; x < currVer; currVer++){
+					tmpMinSumDistance = currVer==0? maxDistance : Integer.MAX_VALUE;
+					for(int x=0; x < currVer; x++){
 						if( tmpMinSumDistance > 
 									tableOfVertexes[x].getDistanceToNeigbour(currNeigh) 
 									+ tableOfVertexes[x].getDistanceToNeigbour(currVer) 
 								)
-							tmpMinSumDistance = tableOfVertexes[x].getDistanceToNeigbour(currNeigh);
+							tmpMinSumDistance = tableOfVertexes[x].getDistanceToNeigbour(currNeigh)
+									+ tableOfVertexes[x].getDistanceToNeigbour(currVer);
 					}
+					tmpMinSumDistance = tmpMinSumDistance <=1000? tmpMinSumDistance : 1000;
+					
 					tableOfVertexes[currVer].setDistanceToNeighbour(currNeigh, random.nextInt(tmpMinSumDistance-1) + 1 );
 				}
 			}
@@ -50,12 +55,13 @@ public class MyGraph2 {
 		for(int i=0; i<tableOfVertexes.length; i++){
 			for(int j=0; j<tableOfVertexes.length; j++)
 				System.out.print( tableOfVertexes[i].getDistanceToNeigbour(j) + "\t");
+			System.out.println("");
 		}
 	}
 
 
 	public static void main(String [] args){
-		MyGraph2 nowy = new MyGraph2(5);
+		MyGraph2 nowy = new MyGraph2(4);
 		nowy.wypisz();
 		
 	}
